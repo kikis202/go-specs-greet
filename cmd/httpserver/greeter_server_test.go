1219df63e1afaea244baae0edf260766f4bc7de0
Kristaps Kozulis <kristaps1000@gmail.com>
2025-12-21 15:01:48 +0200

    Http test passing

--- cmd/httpserver/greeter_server_test.go
+++ cmd/httpserver/greeter_server_test.go
@@ -1,13 +1,39 @@
 package main_test
 
 import (
+	"context"
+	"os"
 	"testing"
 
+	"github.com/alecthomas/assert/v2"
 	go_specs_greet "github.com/kikis202/go-specs-greet/adapters/httpserver"
 	"github.com/kikis202/go-specs-greet/specifications"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:        "../../.",
+			Dockerfile:     "./cmd/httpserver/Dockerfile",
+			BuildLogWriter: os.Stdout,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
 	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
 	specifications.GreetSpecification(t, driver)
 }
