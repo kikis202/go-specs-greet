42e667b47232d0df2668e81e15303ec6de70734f
Kristaps Kozulis <kristaps1000@gmail.com>
2025-12-21 15:56:11 +0200

    Update greeter to greet specific name

--- adapters/handler.go
+++ adapters/handler.go
@@ -3,8 +3,11 @@ package go_specs_greet
 import (
 	"fmt"
 	"net/http"
+
+	go_specs_greet "github.com/kikis202/go-specs-greet/domain-logic"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world")
+	name := r.URL.Query().Get("name")
+	fmt.Fprintf(w, go_specs_greet.Greet(name))
 }
