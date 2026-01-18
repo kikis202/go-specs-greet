06840b17b70b44e962fea5aeeebbf8f853be496e
Kristaps Kozulis <kristaps1000@gmail.com>
2026-01-19 01:03:59 +0200

    simplified http greeter server test

--- cmd/httpserver/greeter_server_test.go
+++ cmd/httpserver/greeter_server_test.go
@@ -1,48 +1,26 @@
 package main_test
 
 import (
-	"context"
+	"fmt"
 	"net/http"
-	"os"
 	"testing"
 	"time"
 
-	"github.com/alecthomas/assert/v2"
+	"github.com/kikis202/go-specs-greet/adapters"
 	"github.com/kikis202/go-specs-greet/adapters/httpserver"
 	"github.com/kikis202/go-specs-greet/specifications"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	ctx := context.Background()
+	var (
+		port           = "8080"
+		dockerFilePath = "./cmd/httpserver/Dockerfile"
+		baseURL        = fmt.Sprintf("http://localhost:%s", port)
+		driver         = httpserver.Driver{BaseURL: baseURL, Client: &http.Client{
+			Timeout: 1 * time.Second,
+		}}
+	)
 
-	req := testcontainers.ContainerRequest{
-		FromDockerfile: testcontainers.FromDockerfile{
-			Context:        "../../.",
-			Dockerfile:     "./cmd/httpserver/Dockerfile",
-			BuildLogWriter: os.Stdout,
-		},
-		ExposedPorts: []string{"8080:8080"},
-		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
-	}
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-
-	assert.NoError(t, err)
-	t.Cleanup(func() {
-		assert.NoError(t, container.Terminate(ctx))
-	})
-
-	client := http.Client{
-		Timeout: 1 * time.Second,
-	}
-
-	driver := httpserver.Driver{
-		BaseURL: "http://localhost:8080",
-		Client:  &client,
-	}
+	adapters.StartDockerServer(t, port, dockerFilePath)
 	specifications.GreetSpecification(t, driver)
 }
